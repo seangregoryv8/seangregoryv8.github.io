f2a28f9dde1d56ac006ffc0b70b852ae7a3a83d6
Sean Gregory <chiefsgreg2622@gmail.com>
2023-05-25 15:01:55 -0400

    Patched up the about me

--- js/index.js
+++ js/index.js
@@ -1,12 +1,8 @@
 let introduction = 
-"I am a John Abbott College computer science student with a keen eye for the arts, storytelling and finite attention to detail. " + 
+"I am a Concordia computer science and computation arts joint major student with a keen eye for the arts, storytelling and finite attention to detail. " + 
 "I always make sure everything I put out is my best work, and never hesitate to sink hundreds of hours into a project to achieve such. I seek " +
 "to use my combination of art and computer skills in whatever I do, in the hopes of fabricating something truly amazing and bigger than myself " + 
-"that I could be proud of for the rest of my life.\n\n" + 
-"Born and raised in Montreal Quebec in a quiet town, I'm all for the suburban, quiet lifestyle, but because I'm an extrovert, I wouldn't mind a bit of chaos in my " + 
-"life. My mind is always flowing with ideas, so I could probably be seen in a coffee shop (even though I don't drink coffee), a park, or just in front of my computer, " +
-"trying to take all the insane ideas from my head and find some use for them.\n\n" + 
-"I have always focused a lot on finite detail, and hope to use my combined set of artistic and computer skills to find a job where I can show people just what I can do."
+"that I could be proud of for the rest of my life.\n\n"
 
 function PreparePage()
 {
